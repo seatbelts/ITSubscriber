069091713b1606f599046f2565e48a435f8b3982
LuckyMagpie <diego.sse7@gmail.com>
2016-03-25 19:16:15 -0600

    Models

---
+++ subscriber/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Evento)
+admin.site.register(Projecto)
+admin.site.register(Alumno)
+admin.site.register(Equipo)
---
+++ subscriber/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+MATERIAS = (
+    ('IHC', 'Interaccion Humano Computadora'),
+    ('DM', 'Dispositivos Moviles'),
+    ('CI', 'Computo Integrado'),
+)
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s' %self.nombre
+
+class Projecto(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=255)
+    materia = models.CharField(choices=MATERIAS, max_length=5)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    mesa = models.CharField(max_length=3)
+
+    def __str__(self):
+        return '%s' %self.nombre
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=255)
+    matricula = models.IntegerField(unique=True, db_index=True)
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return '%d - %s' %(self.matricula, self.nombre)
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=30)
+    credentials = models.OneToOneField(User)
+    lider = models.OneToOneField(Alumno, related_name='lider')
+    integrantes = models.ManyToManyField(Alumno, related_name='integrantes')
+
+    def __str__(self):
+        return '%s - Lider: %s' %(self.nombre, self.lider.nombre)
+
+
