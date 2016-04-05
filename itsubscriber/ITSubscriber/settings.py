0fed282d971748553b843b39756d50de2375abe1
LuckyMagpie <diego.sse7@gmail.com>
2016-04-05 00:20:09 -0500

    heroku

--- itsubscriber/ITSubscriber/settings.py
+++ itsubscriber/ITSubscriber/settings.py
@@ -79,17 +79,8 @@ WSGI_APPLICATION = 'ITSubscriber.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'its',
-        'USER': 'diego',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '5432',
-    }
-}
-
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
