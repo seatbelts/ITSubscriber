7c9912dccfbd0dacdaea7c7264a40b92dabf7bf2
Fernando Contreras <fercreek@gmail.com>
2016-05-10 00:15:41 -0500

    Return my projects in user account

--- itsubscriber/subscriber/views.py
+++ itsubscriber/subscriber/views.py
@@ -29,10 +29,10 @@ class ProyectoViewSet(ModelViewSet):
     # serializer_class = ProyectoSerializer
     # permission_classes = (ProyectoPermission, )
 
-    # def get_queryset(self):
-    #     if self.request.user.is_staff:
-    #         return Proyecto.objects.all()
-    #     return Proyecto.objects.filter(equipo__usuario=self.request.user)
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Proyecto.objects.all()
+        return Proyecto.objects.filter(equipo__usuario=self.request.user)
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
